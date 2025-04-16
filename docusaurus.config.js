e13ad2fbca7857bc5cff05daee88e408272a3b6e
Pedro Guerreiro <pmguerre@ualg.pt>
2025-04-16 12:01:16 +0100

    modifiquei titulo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Dokku',
+  tagline: 'Criar sites com o Docusdaurus é muito fácil',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
